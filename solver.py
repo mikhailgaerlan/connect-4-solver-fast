5808a2a1dc4fde493ae4af9a952d5b98ce05ba3b
Mikhail Gaerlan <mikhailgaerlan@gmail.com>
2024-09-13 21:27:16 -0700

    Renamed position to board.

--- solver.py
+++ solver.py
@@ -2,11 +2,11 @@ from .board import Board
 
 __all__ = ['solve']
 
-def solve(position, weak=False):
-    if position.can_win_next:
-        return (Board.max_moves + 1 - position.moves) // 2
-    min_score = -((Board.max_moves - position.moves) // 2)
-    max_score = (Board.max_moves + 1 - position.moves) // 2
+def solve(board, weak=False):
+    if board.can_win_next:
+        return (Board.max_moves + 1 - board.moves) // 2
+    min_score = -((Board.max_moves - board.moves) // 2)
+    max_score = (Board.max_moves + 1 - board.moves) // 2
     if weak:
         min_score = -1
         max_score = 1
@@ -14,38 +14,38 @@ def solve(position, weak=False):
     while min_score < max_score:
         med_score = min_score + (max_score - min_score) // 2
         if med_score <= 0 and min_score // 2 < med_score: med_score = min_score // 2
-        elif med_score >= 0 and max_score // 2 >= med_score: med_score = max_score // 2
-        r = negamax(position, med_score, med_score + 1)
+        elif med_score >= 0 and max_score // 2 > med_score: med_score = max_score // 2
+        r = negamax(board, med_score, med_score + 1)
         if r <= med_score: max_score = r
         else: min_score = r
     return min_score
 
 NEGAMAX_CACHE = {}
 NODE_COUNT = 0
-def negamax(position, alpha, beta):
+def negamax(board, alpha, beta):
     assert alpha < beta
-    assert not position.can_win_next
+    assert not board.can_win_next
 
     global NODE_COUNT
     NODE_COUNT += 1
     
-    if position.possible_non_losing_moves == 0:
-        return -((Board.max_moves - position.moves) // 2)
+    if board.possible_non_losing_moves == 0:
+        return -((Board.max_moves - board.moves) // 2)
     
-    if position.moves >= Board.max_moves - 2:
+    if board.moves >= Board.max_moves - 2:
         return 0
     
-    min_score = -((Board.max_moves - 2 - position.moves) // 2)
+    min_score = -((Board.max_moves - 2 - board.moves) // 2)
     if alpha < min_score:
         alpha = min_score
         if alpha >= beta: return alpha
     
-    max_score = (Board.max_moves - 1 - position.moves) // 2
+    max_score = (Board.max_moves - 1 - board.moves) // 2
     if beta > max_score:
         beta = max_score
         if alpha >= beta: return beta
     
-    bound = NEGAMAX_CACHE.get(position.key, 0)
+    bound = NEGAMAX_CACHE.get(board.key, 0)
     if bound:
         if bound > Board.max_score - Board.min_score + 1:
             min_score = bound + 2 * Board.min_score - Board.max_score - 2
@@ -60,20 +60,20 @@ def negamax(position, alpha, beta):
     
     moves = MoveSorter()
     for col in Board.column_order[::-1]:
-        move = position.possible_non_losing_moves & position.column_mask[col]
-        if move: moves.insert_move(move, position.move_score(move))
+        move = board.possible_non_losing_moves & board.column_mask[col]
+        if move: moves.insert_move(move, board.move_score(move))
     
     while moves:
         next_move = moves.pop().move
-        position2 = position.play_move(next_move)
-        score = -negamax(position2, -beta, -alpha)
+        board2 = board.play_move(next_move)
+        score = -negamax(board2, -beta, -alpha)
 
         if score >= beta:
-            NEGAMAX_CACHE[position.key] = score + Board.max_score - 2 * Board.min_score + 2
+            NEGAMAX_CACHE[board.key] = score + Board.max_score - 2 * Board.min_score + 2
             return score
         if score > alpha: alpha = score
     
-    NEGAMAX_CACHE[position.key] = alpha - Board.min_score + 1
+    NEGAMAX_CACHE[board.key] = alpha - Board.min_score + 1
     return alpha
 
 class MoveSorter(list):
